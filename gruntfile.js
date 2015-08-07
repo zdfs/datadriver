97d484fc37fcc7a31ce4632775a1cae8c7512ac3
Zachary Forrest y Salazar <zforrest@gmail.com>
2015-08-06 18:06:27 -0700

    remove the setupGrunt method
    - This was a stupid idea.

--- gruntfile.js
+++ gruntfile.js
@@ -4,23 +4,26 @@ var gruntConfig = {
 
 		options: {
 
-			browserstack: {
-				user: 'zacharyforrest4',
-				key: 'ZR7h2zB7UKo1RAasrUzt'
-			},
+			// For SauceLabs
+			// ================================
+			// host: 'ondemand.saucelabs.com',
+			// port: 80,
+			// user: 'zdfs',
+			// key: '896a5f44-a86f-4909-bde4-b6e5ec584f00',
+			// updateSauceJob: true
 
-			saucelabs: {
-				user: 'zdfs',
-				key: '896a5f44-a86f-4909-bde4-b6e5ec584f00',
-				updateSauceJob: true
-			},
+			// For BrowserStack
+			// ================================
+			// host: 'hub.browserstack.com',
+			// port: 80,
+			// user: 'zacharyforrest4',
+			// key: 'ZR7h2zB7UKo1RAasrUzt'
 
 			desiredCapabilities: {
 				browserName: 'chrome',
-				browserKey: 'chrome'
-			},
-
-			name: "Documentation Tests"
+				browserKey: 'chrome', // Let's see if we can remove this.
+				name: 'Documentation Tests'
+			}
 
 		},
 
