b113c62e38cc2343b7e599c12b6670514516874c
Zachary Forrest y Salazar <zforrest@gmail.com>
2015-08-08 16:36:21 -0700

    Remove all the documentation shiz from the dev branch

--- assets/scripts/app/grunticon.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	grunticon([
-		"images/icons/icons.data.svg.css",
-		"images/icons/icons.data.png.css",
-		"images/icons/icons.fallback.css"
-	], grunticon.svgLoadedCallback);
-});
--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/drive');
+module.exports = require('./lib/datadriver');
