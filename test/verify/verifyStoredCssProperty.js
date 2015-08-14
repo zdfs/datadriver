a724d974265210c02c95f20591d607e94de9ed49
Zachary Forrest y Salazar <zforrest@gmail.com>
2015-08-14 16:22:51 -0700

    Add documentation for the getStoredCssProperty method

---
+++ test/verify/verifyStoredCssProperty.js
@@ -0,0 +1,91 @@
+'use strict';
+
+var drive = require('datadriver');
+
+drive.verify({
+
+  "steps": [
+
+		{
+			"execute": [
+				{
+					"action": "redirect",
+					"page": "api/verify/verifyStoredCssProperty.html"
+				}
+			]
+		},
+
+		{
+			"selector": ".#column1",
+			"execute": [
+				{
+					"action": "evalAndStore",
+					"script": "return Math.round(parseInt(window.getComputedStyle(document.querySelector('#column1')).width, 10))",
+					"resultKey": "column1-width"
+				}
+			]
+		},
+
+		{
+			"selector": "#column2",
+			"verify": [
+				{
+					"method": "verifyStoredCssProperty",
+					"asserts": {
+						"width": "column1-width"
+					}
+				}
+			]
+		},
+
+		{
+			"execute": [
+				{
+					"action": "setViewportSize",
+					"height": 1000,
+					"width": 400
+				}
+			]
+		},
+
+		{
+			"selector": ".#column1",
+			"execute": [
+				{
+					"action": "evalAndStore",
+					"script": "return Math.round(parseInt(window.getComputedStyle(document.querySelector('#column1')).width, 10))",
+					"resultKey": "column1-width"
+				}
+			]
+		},
+
+		{
+			"selector": "#column2",
+			"verify": [
+				{
+					"method": "verifyStoredCssProperty",
+					"asserts": {
+						"width": "column1-width"
+					}
+				}
+			]
+		}
+
+  ]
+
+});
+
+drive.execute({
+	"execute": {
+		"action": "evalAndStore",
+		"script": "return Math.round(parseInt(window.getComputedStyle(document.querySelector('#column1')).width, 10))",
+		"resultKey": "column1-width"
+	}
+});
+
+drive.verifyStoredCssProperty({
+  selector: '#column2',
+  assertKey: 'width',
+  assert: 'column1-width',
+  mode: [ 'equal' ]
+});
