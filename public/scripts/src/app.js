2069edd280491ea2367aec5e5fdae335795a1d63
Zachary Forrest y Salazar <zforrest@gmail.com>
2015-08-15 20:58:58 -0700

    Add documentation for verifyScriptResult() method

--- public/scripts/src/app.js
+++ public/scripts/src/app.js
@@ -9709,3 +9709,15 @@ $(document).ready(function() {
 	});
 
 });
+$(document).ready(function() {
+  $(".dropdown-button").click(function() {
+    var $button, $menu;
+    $button = $(this);
+    $menu = $button.siblings(".dropdown-menu");
+    $menu.toggleClass("show-menu");
+    $menu.children("li").click(function() {
+      $menu.removeClass("show-menu");
+      $button.html($(this).html());
+    });
+  });
+});
