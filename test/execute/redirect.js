964de4bdce6d4f972283e6d9dede04171c0f2f0a
Zachary Forrest y Salazar <zforrest@gmail.com>
2015-08-18 14:49:58 -0700

    Add redirect() documentation

--- test/execute/redirect.js
+++ test/execute/redirect.js
@@ -3,8 +3,22 @@
 var drive = require('datadriver');
 
 drive.execute({
+	"execute": {
+		"action": "redirect",
+		"page": "api/execute/redirect.html"
+	}
+});
+
+drive.execute({
   "execute": {
     "action": "redirect",
     "page": "http://google.com"
+  }
+});
+
+drive.execute({
+  "execute": {
+    "action": "redirect",
+    "page": "api/execute/redirect.html"
   }
 });
