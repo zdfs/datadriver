ab0bd4e4d6be9eb0d8f31d96e0ba72be683df10e
Zachary Forrest y Salazar <zforrest@gmail.com>
2015-08-20 11:33:46 -0700

    #3 - Remove chai-as-promised, add first unit tests
    We don’t need chai-as-promised at the moment, so I’m removing it as a
    dependency. I’ve also documented the shit out of the setup method, and
    added tape for unit testing.

---
+++ tests/unit/setup.js
@@ -0,0 +1,15 @@
+var test = require('tape');
+var drive = require('../../lib/datadriver');
+
+test('setup() method', function(t) {
+
+	var setup = drive.setup();
+
+	t.notEqual(browser, undefined, 'The global browser object should be defined.');
+	t.notEqual(browser.executeAndStore, undefined, 'The browser object should have an executeAndStore() method.');
+	t.notEqual(setup.chai, undefined, 'The Chai assertion object should be defined.');
+	t.notEqual(setup.chai.should, undefined, 'The should() interface for Chai should be defined.');
+
+	t.end();
+
+});
