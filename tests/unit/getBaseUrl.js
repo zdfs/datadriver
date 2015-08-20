7f9cf71721995ea36a6483bba4040781bca45eb3
Zachary Forrest y Salazar <zforrest@gmail.com>
2015-08-20 11:57:23 -0700

    #4 Add unit tests for getBaseUrl() method.

---
+++ tests/unit/getBaseUrl.js
@@ -0,0 +1,22 @@
+var test = require('tape');
+var drive = require('../../lib/datadriver');
+
+test('getBaseUrl() method', function(t) {
+
+ 	var baseUrl;
+
+ 	process.argv.push("--url=http://test.com");
+
+	baseUrl = drive.getBaseUrl();
+
+	t.equal(baseUrl, "http://test.com/", 'The url should equal http://test.com/ (with slash) with url option.');
+
+	process.argv.pop();
+
+	baseUrl = drive.getBaseUrl();
+
+	t.equal(baseUrl, "http://localhost:3000/", 'The url should equal http://localhost:3000/ (with slash) without url option.');
+
+	t.end();
+
+});
